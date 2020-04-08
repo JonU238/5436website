681fc395c03c7de1228a10f94fc9ee889bf792e7
Natick5436 <mali@students.natickps.org>
2020-04-08 15:13:33 -0400

    added and changed some content

--- js/global.js
+++ js/global.js
@@ -1,3 +1,8 @@
 $("#menu").click(function() {
-  $("nav").hasClass("responsive") ? $("nav").removeClass("responsive") : $("nav").addClass("responsive")
+  if ($("nav").hasClass("responsive")) {
+    $("nav, html").removeClass("responsive");
+  } else {
+    $("nav, html").addClass("responsive")
+  }
 })
+$("html").append("<a id='corona' href='volunteering.html#coronaEfforts'>COVID-19 Efforts</a>")
